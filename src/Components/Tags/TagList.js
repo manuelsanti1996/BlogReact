94d6963486eecf6bb361738e181bab6f67947403
Manuel Santi <manuelsanti19961996@gmail.com>
2023-03-28 17:10:21 +0200

    commento

--- src/Components/Tags/TagList.js
+++ src/Components/Tags/TagList.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import Tag from './Tag';
+import { articleData } from '../../data';
 
-const TagList = ({ tags, onTagSelected }) => {
+const TagList = ({ tags: articleTags, onTagSelected }) => {
+  console.log("TIPO VARIABILE: ", typeof articleTags);
+
+  const tags = [...new Set(articleData.map(article => article.tag))];
+
   return (
     <div>
+
       {tags.map((tag) => (
         <Tag key={tag}
-             tag={tag}
-             onTagSelected={onTagSelected}
+          tag={tag}
+          onTagSelected={onTagSelected}
         />
       ))}
     </div>
@@ -16,3 +22,9 @@ const TagList = ({ tags, onTagSelected }) => {
 
 export default TagList;
 
+
+/* new Set è un oggetto JavaScript che rappresenta una collezione di valori univoci,
+ dove ogni valore può comparire solo una volta.
+
+articleData.map(article => article.tag) crea un nuovo array che contiene solo i valori dell'attributo "tag" 
+per ogni oggetto all'interno dell'array articleData.*/
