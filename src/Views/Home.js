a0ec9c7dbc01c5a4e73bd7823cb2946b16792914
Manuel Santi <manuelsanti19961996@gmail.com>
2023-03-28 16:22:43 +0200

    correzione immagini

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
   return (
     <div>
       <HeaderNavBar />
-      <CardHero title="Blog" description="descrizione della card" bgimage="./sfondo.jpeg" />
+      <CardHero title="Blog" description="descrizione della card" bgimage="././images/sfondo.jpeg" />
       <p className='font-semibold mx-4'> Ultime Notizie </p>
       <CardsList />
       <p className='font-semibold mx-4 my-4'>Archivio Notizie </p>
--- src/data.js
+++ src/data.js
@@ -4,63 +4,63 @@ export const articleData = [
     {
         title: "Articolo 1 ",
         description: "lorem ipsum",
-        image: "th.jpg",
+        image: "./images/th.jpg",
         tag:TAGS.TAG1,
         maxwidth: "md"
     },
     {
         title: "Articolo 2",
         description: "sed id faciam",
-        image: "images.jpg",
+        image: "./images/images.jpg",
         tag:TAGS.TAG2,
         maxwidth: "md"
     },
     {
         title: "Articolo 3",
         description: "iniquit",
-        image: "cardarticle.jpg",
+        image: "./images/cardarticle.jpg",
         tag:TAGS.TAG3,
         maxwidth: "md"
     },
     {
         title: "Articolo 4 ",
         description: "lorem ipsum",
-        image: "pompa-idraulica_NG1.jpg",
+        image: "./images/pompa-idraulica_NG1.jpg",
         tag:TAGS.TAG1,
         maxwidth: "md"
     },
     {
         title: "Articolo 5",
         description: "sed id faciam",
-        image: "images.jpg",
+        image: "./images/images.jpg",
         tag:TAGS.TAG4,
         maxwidth: "md"
     },
     {
         title: "Articolo 6",
         description: "iniquit",
-        image: "po.gif",
+        image: "./images/po.gif",
         tag:TAGS.TAG1,
         maxwidth: "md"
     },
     {
         title: "Articolo 7 ",
         description: "d",
-        image: "c.png",
+        image: "./images/c.png",
         tag:TAGS.TAG4,
         maxwidth: "md"
     },
     {
         title: "Articolo 8",
         description: "b",
-        image: "images.jpg",
+        image: "./images/images.jpg",
         tag:TAGS.TAG2,
         maxwidth: "md"
     },
     {
         title: "Articolo 9",
         description: "c",
-        image: "cardarticle.jpg",
+        image: "./images/cardarticle.jpg",
         tag:TAGS.TAG1,
         maxwidth: "md"
     },
