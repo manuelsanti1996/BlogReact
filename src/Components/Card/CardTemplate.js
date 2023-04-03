253ed72af9317004d8582c4651c9eba6221b4ae1
Manuel <manuelsanti.dev1996@gmail.com>
2023-04-03 21:59:42 +0200

    cardtempl

--- src/Components/Card/CardTemplate.js
+++
@@ -1,9 +0,0 @@
-const CardTemplate = ({ image }) => {
-
-    return (
-        <div >
-            <img className='h-40 object-cover rounded-xl' src={image} />
-        </div>
-    )
-}
-export default CardTemplate
